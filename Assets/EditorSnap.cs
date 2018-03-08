373b52f57ee8ccd74688779a6053bc1f2037be11
erik123567 <erik123567@gmail.com>
2018-03-08 15:36:45 -0500

    Initial commit

---
+++ Assets/EditorSnap.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+[ExecuteInEditMode]
+public class EditorSnap : MonoBehaviour {
+    [SerializeField] [Range(1f,20f)] float gridSize = 10f;
+
+    private void Awake()
+    {
+        Debug.Log("here");
+    }
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Vector3 snapPos;
+        snapPos.x = Mathf.RoundToInt(transform.position.x / gridSize) * gridSize;
+        snapPos.z = Mathf.RoundToInt(transform.position.z / gridSize) * gridSize;
+        snapPos.y = 0f;
+        transform.position = snapPos;
+    }
+}
+//dude
